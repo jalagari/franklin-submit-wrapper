fb7d3d0222e73c85ae9f1a8eb372f6dde3e60db6
neeraj <neerajadobe93@gmail.com>
2023-06-15 17:23:11 +0530

    removed unneccesary indentation

--- src/Cache.js
+++ src/Cache.js
@@ -1,28 +1,29 @@
 export default class Cache {
-  /**
-   * Cloudflare KeyValue Namespace
-   */
-  namespace;
 
-  constructor(namespace) {
-    this.namespace = namespace;
-  }
+    /**
+     * Cloudflare KeyValue Namespace
+     */
+    namespace;
 
-  async put(key, data, ttl) {
-    if (ttl) {
-      await this.namespace?.put(key, JSON.stringify(data), {
-        expirationTtl: ttl,
-      });
-    } else {
-      await this.namespace?.put(key, JSON.stringify(data));
+    constructor(namespace) {
+        this.namespace = namespace;
     }
-  }
 
-  async get(key) {
-    let data = await this.namespace?.get(key);
-    if (data) {
-      return JSON.parse(data);
+    async put(key, data, ttl) {
+        if (ttl) {
+          await this.namespace?.put(key, JSON.stringify(data), {
+            expirationTtl: ttl,
+          });
+        } else {
+          await this.namespace?.put(key, JSON.stringify(data));
+        }
     }
-    return {};
-  }
+
+    async get (key) {
+        let data = await this.namespace?.get(key);
+        if (data) {
+            return JSON.parse(data);
+        }
+        return {};
+    }
 }
